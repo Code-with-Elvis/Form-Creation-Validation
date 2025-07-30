a809430020c6ec9c1afaf0c2c6a2ec7d42f527ad
Code-with-Elvis <okothelvis137@gmail.com>
2025-07-30 04:31:14 -0600

    fix: Outside fetchUserData, add an event listener to document for the DOMContentLoaded event.

--- fetch-data.js
+++ fetch-data.js
@@ -1,34 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
+const fetchUserData = async () => {
+  const apiUrl = "https://jsonplaceholder.typicode.com/users";
   const dataContainer = document.getElementById("api-data");
 
-  const fetchUserData = async () => {
-    const apiUrl = "https://jsonplaceholder.typicode.com/users";
-    try {
-      const response = await fetch(apiUrl);
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      const users = await response.json();
-      dataContainer.innerHTML = "";
-
-      const userList = document.createElement("ul");
-
-      users.forEach((user) => {
-        const listItem = document.createElement("li");
-        listItem.textContent = `${user.name}`;
-        userList.appendChild(listItem);
-      });
-
-      dataContainer.appendChild(userList);
-    } catch (error) {
-      dataContainer.innerHTML = "";
-      dataContainer.textContent = "Failed to load user data.";
-      console.error(
-        "There has been a problem with your fetch operation:",
-        error
-      );
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
-  };
+    const users = await response.json();
+    dataContainer.innerHTML = "";
 
-  fetchUserData();
-});
+    const userList = document.createElement("ul");
+
+    users.forEach((user) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = `${user.name}`;
+      userList.appendChild(listItem);
+    });
+
+    dataContainer.appendChild(userList);
+  } catch (error) {
+    dataContainer.innerHTML = "";
+    dataContainer.textContent = "Failed to load user data.";
+    console.error("There has been a problem with your fetch operation:", error);
+  }
+};
+
+document.addEventListener("DOMContentLoaded", fetchUserData);
